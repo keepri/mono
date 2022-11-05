02cf46ab4e20a5c2b7340903fcbf77b961d028b6
cpitul <ciprianpitul@gmail.com>
2022-11-05 21:10:05 +0200

    FOLEY: changed index bg color

--- apps/foley/src/pages/index.tsx
+++ apps/foley/src/pages/index.tsx
@@ -1,16 +1,17 @@
 import { URLS } from '@declarations/enums';
+import styles from '@styles/Index.module.scss';
 import { NextPage } from 'next';
 import Link from 'next/link';
 
 const IndexPage: NextPage = () => {
 	return (
 		<>
-			<section className="grid place-content-center place-items-center gap-2 min-h-screen">
-				<h1 className="relative font-londrina-sketch text-9xl leading-tight">
+			<section className="grid place-content-center place-items-center gap-2 min-h-screen bg-clr-bg-300">
+				<h1 style={{ color: 'var(--clr-white)' }} className="relative font-londrina-sketch text-9xl leading-tight">
 					KIPRI
 					<span className="text-2xl">.dev</span>
 				</h1>
-				<Link className="button" href={URLS.REPLACE}>
+				<Link className={`button ${styles.boop}`} href={URLS.REPLACE}>
 					BOOP
 				</Link>
 			</section>
