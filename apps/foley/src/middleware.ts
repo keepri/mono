6d2d287641917c36114fb17791ee1b7141ffed18
cpitul <ciprianpitul@gmail.com>
2023-01-24 19:35:12 +0200

    foley: added error case to smol when upstash blocks requests

--- apps/foley/src/middleware.ts
+++ apps/foley/src/middleware.ts
@@ -4,7 +4,7 @@ import { Ratelimit } from '@upstash/ratelimit';
 import { Redis } from '@upstash/redis';
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
-const ratelimit = new Ratelimit({ redis: Redis.fromEnv(), limiter: Ratelimit.fixedWindow(200, '5 s') });
+const ratelimit = new Ratelimit({ redis: Redis.fromEnv(), limiter: Ratelimit.fixedWindow(7, '10 s') });
 
 export default async function handler(req: NextRequest, ev: NextFetchEvent) {
 	const ip = req.ip ?? '127.0.0.1';
