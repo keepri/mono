48ab4d17d7c61cf613331f51a48738f0cbea187f
cpitul <ciprianpitul@gmail.com>
2023-02-07 13:04:41 +0200

    @clfxc/services: added Prettify type helper

--- packages/services/utils/types.ts
+++ packages/services/utils/types.ts
@@ -1,6 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type AsyncReturnType<T extends (...args: any) => Promise<any>> = T extends (...args: any) => Promise<infer R>
-	? R
-	: unknown;
+  ? R
+  : unknown;
 
 export type WithRequired<Type, Key extends keyof Type> = Omit<Type, Key> & Required<Pick<Type, Key>>;
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type Prettify<T> = { [K in keyof T]: T[K]; } & {};
+
