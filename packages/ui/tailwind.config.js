06ecd0b9f4994f2eaa747e45739a4fa38e70524c
cpitul <ciprianpitul@gmail.com>
2023-02-09 19:38:45 +0200

    added all js file to linter, where there are any

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { join } = require('path');
+const { join } = require("path");
 module.exports = {
-	content: [join(__dirname, 'components', '**', '*.{js,ts,jsx,tsx}')],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: [join(__dirname, "components", "**", "*.{js,ts,jsx,tsx}")],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
