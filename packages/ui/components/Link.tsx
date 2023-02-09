b1457e7638a65294d34594591076a414336b6d17
cpitul <ciprianpitul@gmail.com>
2023-02-09 19:23:53 +0200

    fixed prod build

--- packages/ui/components/Link.tsx
+++ packages/ui/components/Link.tsx
@@ -1,7 +1,11 @@
 import { default as NLink } from "next/link";
 import { AnchorHTMLAttributes, FC, PropsWithChildren } from "react";
+import { UrlObject } from "url";
 
-interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> { }
+type Url = string | UrlObject;
+interface Props extends Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "href"> {
+    href: Url;
+}
 
 export const Link: FC<PropsWithChildren<Props>> = ({ children, className, ...rest }) => {
     return (
