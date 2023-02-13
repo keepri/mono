57dd7d8f0de56a9af65d5d8e1fa9c7a08b3e0272
cpitul <ciprianpitul@gmail.com>
2023-02-13 09:27:00 +0200

    foley: added vercel analytics

--- apps/foley/src/pages/_app.tsx
+++ apps/foley/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@styles/font-faces.scss";
 // eslint-disable-next-line import/no-unresolved
 import "@styles/globals.scss";
 // import '@styles/reset.scss';
+import { Analytics } from "@vercel/analytics/react";
 
 import Layout from "@components/Layout/Layout";
 import { AppProps } from "next/app";
@@ -9,9 +10,12 @@ import type { NextPage } from "next/types";
 
 const KIPRI: NextPage<AppProps> = ({ Component, pageProps }) => {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+            <Analytics />
+        </>
     );
 };
 
