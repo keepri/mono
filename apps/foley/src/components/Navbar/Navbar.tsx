cdf2f85247d4a367c8e9ce7f3069e3d4524edbc6
KIPRI <hi@kipri.dev>
2023-02-25 13:08:01 +0200

    foley: made navbar padding smaller

--- apps/foley/src/components/Navbar/Navbar.tsx
+++ apps/foley/src/components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import { FC, HTMLAttributes } from "react";
 type Props = HTMLAttributes<HTMLElement>;
 const Navbar: FC<Props> = ({ className, ...rest }) => {
     return (
-        <nav className={`container min-h-[4rem] flex justify-between gap-4 px-4 py-5 bg-[var(--clr-bg-500)] ${inconsolata.variable} font-inconsolata ${className ? className : ""}`} {...rest}>
+        <nav className={`container min-h-[4rem] flex justify-between gap-4 p-2 bg-[var(--clr-bg-500)] ${inconsolata.variable} font-inconsolata ${className ? className : ""}`} {...rest}>
             <Link plain href="/" className="px-4">
                 <h1 className={`${londrinaSketch.variable} font-londrina-sketch hover:text-[var(--clr-orange)] text-white text-6xl`}>K</h1>
             </Link>
