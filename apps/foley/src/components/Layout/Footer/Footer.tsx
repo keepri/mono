b85cfc187a0971a4ea319dde0bc86770231fa2f3
KIPRI <hi@kipri.dev>
2023-02-25 12:42:32 +0200

    foley: footer padding lowered

--- apps/foley/src/components/Layout/Footer/Footer.tsx
+++ apps/foley/src/components/Layout/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import { FC } from "react";
 
 const Footer: FC = () => {
     return (
-        <footer className="flex justify-center items-center gap-4 p-4 bg-[var(--clr-bg-500)]">
+        <footer className="flex justify-center items-center gap-4 p-2 bg-[var(--clr-bg-500)]">
             <Link plain href="https://github.com/keepri" className={`flex gap-1 items-center justify-evenly py-1 px-2 text-xs hover:!text-[var(--clr-orange)] !text-white ${inconsolata.variable} font-inconsolata`} target="_blank">
                 <GitHub width="25px" height="25px" />
                 github/keepri
