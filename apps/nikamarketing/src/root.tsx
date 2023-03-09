df50ca7d59286c0fa81d926f90be0e62ae579097
KIPRI <hi@kipri.dev>
2023-03-09 16:12:41 +0200

    nika: updated trpc & peer deps for trpc

--- apps/nikamarketing/src/root.tsx
+++ apps/nikamarketing/src/root.tsx
@@ -1,7 +1,7 @@
 // @refresh reload
 import { Suspense, type JSXElement } from "solid-js";
 import { Body, ErrorBoundary, FileRoutes, Head, Html, Link, Meta, Route, Routes, Scripts, Title } from "solid-start";
-import { queryClient, trpc } from "~/utils/trpc";
+import { client, queryClient, trpc } from "~/utils/trpc";
 import NotFound from "./components/NotFound";
 import "./root.css";
 
@@ -17,7 +17,8 @@ export default function Root(): JSXElement {
                 <Link rel="icon" href="/favicon.ico" />
             </Head>
             <Body>
-                <trpc.Provider queryClient={queryClient}>
+                {/** @ts-expect-error xD! */}
+                <trpc.Provider client={client} queryClient={queryClient}>
                     <Suspense>
                         <ErrorBoundary>
                             <Routes>
--- apps/nikamarketing/src/utils/trpc.ts
+++ apps/nikamarketing/src/utils/trpc.ts
@@ -1,23 +1,20 @@
-import { QueryClient } from "@adeora/solid-query";
+import { QueryClient } from "@tanstack/solid-query";
 import { httpBatchLink, loggerLink } from "@trpc/client";
-import { createTRPCSolidStart } from "solid-trpc";
+import { createTRPCSolid } from "solid-trpc";
 import { type AppRouter } from "~/server/trpc/router/_app";
 
 export const BASE_TRPC_PATHNAME = "/api/trpc" as const;
 
 const getBaseUrl = () => {
     if (typeof window !== "undefined") return "";
-    // TODO PROD: replace example.com with your actual production url
     if (process.env.NODE_ENV === "production") return `https://nikamarketing.vercel.app${BASE_TRPC_PATHNAME}`;
     return `http://localhost:3000${BASE_TRPC_PATHNAME}`;
 };
 
-export const trpc = createTRPCSolidStart<AppRouter>({
-    config() {
-        return {
-            links: [loggerLink(), httpBatchLink({ url: getBaseUrl() })],
-        };
-    },
+export const trpc = createTRPCSolid<AppRouter>();
+
+export const client = trpc.createClient({
+    links: [loggerLink(), httpBatchLink({ url: getBaseUrl() })],
 });
 
 export const queryClient = new QueryClient();
