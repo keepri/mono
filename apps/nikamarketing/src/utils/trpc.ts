efb7a03499160ff589a4e860426c7776f91aaa97
KIPRI <hi@kipri.dev>
2023-03-09 13:48:57 +0200

    updated git ignore

--- apps/nikamarketing/src/utils/trpc.ts
+++ apps/nikamarketing/src/utils/trpc.ts
@@ -8,7 +8,7 @@ export const BASE_TRPC_PATHNAME = "/api/trpc" as const;
 const getBaseUrl = () => {
     if (typeof window !== "undefined") return "";
     // TODO PROD: replace example.com with your actual production url
-    if (process.env.NODE_ENV === "production") return `https://example.vercel.app${BASE_TRPC_PATHNAME}`;
+    if (process.env.NODE_ENV === "production") return `https://nikamarketing.vercel.app${BASE_TRPC_PATHNAME}`;
     return `http://localhost:3000${BASE_TRPC_PATHNAME}`;
 };
 
