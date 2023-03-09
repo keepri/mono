d5357eb3d2d7d1cdd7d2c58452ac309e45e3e6c2
KIPRI <hi@kipri.dev>
2023-03-09 10:38:12 +0200

    nika: fixed auth.d.ts Session type declaration

--- apps/nikamarketing/auth.d.ts
+++ apps/nikamarketing/auth.d.ts
@@ -1,31 +1,30 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
-/* eslint-disable @typescript-eslint/ban-types */
 import {
     type DefaultSession,
-    type DefaultUser,
-    type Account as DefaultAccount,
-    type Profile as DefaultProfile,
+    // type DefaultUser,
+    // type Account as DefaultAccount,
+    // type Profile as DefaultProfile,
 } from "@auth/core/types";
 
 declare module "@auth/core/types" {
-    interface Session extends DefaultSession.user {
+    interface Session {
         user?: {
-            id?: string;
-        };
+            id?: string | null;
+        } & DefaultSession["user"];
     }
 
     /**
      * The shape of the user object returned in the OAuth providers' `profile` callback,
      * or the second parameter of the `session` callback, when using a database.
      */
-    interface User extends DefaultUser {}
+    // interface User extends DefaultUser { }
 
     /**
      * Usually contains information about the provider being used
      * and also extends `TokenSet`, which is different tokens returned by OAuth Providers.
      */
-    interface Account extends DefaultAccount {}
+    // interface Account extends DefaultAccount { }
 
     /** The OAuth profile returned from your provider */
-    interface Profile extends DefaultProfile {}
+    // interface Profile extends DefaultProfile { }
 }
