938d6644525bf8f765b101422837f8d348fd991d
KIPRI <hi@kipri.dev>
2023-03-09 11:32:12 +0200

    nika: ui change to trigger ci

--- apps/nikamarketing/src/routes/(home).tsx
+++ apps/nikamarketing/src/routes/(home).tsx
@@ -8,7 +8,7 @@ const AuthShowcase: VoidComponent = (): JSXElement => {
     return (
         <div class="flex flex-col items-center justify-center gap-4">
             <p class="text-center text-2xl text-white">
-                {session() && <span>Logged in as {session()?.user?.name} !</span>}
+                {session() && <span>Logged in as {session()?.user?.name}</span>}
             </p>
             <p class="cursor-pointer rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20">
                 {session() ? "boop" : "boop"}
