5ad2c04e741b68da22638a3b61fbb7d22ab1d2c3
KIPRI <hi@kipri.dev>
2023-05-11 21:09:05 +0300

    foley: removed session validation for smol/slug api route

--- apps/foley/src/pages/api/smol/[slug].ts
+++ apps/foley/src/pages/api/smol/[slug].ts
@@ -1,15 +1,10 @@
-import { getSmolBySlug, validateSession } from "@utils/helpers";
+import { getSmolBySlug } from "@utils/helpers";
 import type { NextApiRequest, NextApiResponse } from "next/types";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const slug = req.query["slug"];
     if (typeof slug !== "string") {
         return res.status(400).json({ message: "slug not valid" });
-    }
-
-    const session = await validateSession(req.headers);
-    if (!session) {
-        return res.status(401).json({ message: "could not validate session" });
     }
 
     const smolRes = await getSmolBySlug(slug);
